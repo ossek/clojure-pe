396c97a15580b700124f963cefe8e8f951a44ce4
ossek <guy.20.sherlocken@spamgourmet.com>
2015-01-01 22:23:31 -0600

    sln 1

---
+++ p1/src/p1/core.clj
@@ -0,0 +1,20 @@
+(ns p1.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+;;sum of all multiples of 3 or 5 below 1000
+;;aka fizzbuzz
+((defn sumMult
+  []
+  (loop [i 0
+         sum 0]
+    (if (< i 1000) 
+        (if (or (= (mod i 5) 0) (= (mod i 3) 0))
+            (recur (inc i) (+ sum i) ) 
+            (recur (inc i) sum))
+        sum))) )
+
