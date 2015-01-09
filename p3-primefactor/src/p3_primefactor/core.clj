aa141fd40c40a7f39bc98eeafed76e62bba361e7
ossek <github.20.sherlocken@spamgourmet.com>
2015-01-08 23:38:23 -0600

    attempting segment

--- p3-primefactor/src/p3_primefactor/core.clj
+++ p3-primefactor/src/p3_primefactor/core.clj
@@ -6,35 +6,6 @@
   [& args]
   (println "Hello, World!"))
 
-(clojure.math.numeric-tower/expt 600851475143 0.5)
-(clojure.math.numeric-tower/expt 200 0.5)
-
-(defn checkSegment
-  [primesUpToSqrtMax segmentStart segmentEnd]
-  (loop [[prime & remain] primesUpToSqrtMax 
-           final-segment (filter odd? (range segmentStart segmentEnd))]
-      (if (nil? prime)
-        final-segment
-        (recur remain (crossOffMultiples prime final-segment)))))
-
-(checkSegment [2,3,5,7] 3 45)
-
-;;cross off multiples of p starting from p^.  assuming that segment is odds only
-(defn crossOffMultiples
-  [p segment]
-  (loop [check (* p p)
-         crossed-segment segment]
-    (if (<= check (last segment))
-     (if (= (mod check p) 0)
-       (recur (+ check 2) (remove (fn [x] (= x check)) crossed-segment))
-       (recur (+ check 2) crossed-segment))
-     crossed-segment)))
-
-(crossOffMultiples 5 (filter odd? (range 10 80)))
-
-;;GC Error
-(take 12 (range 99999999))
-
 ;;there may be some lazy 
 ;;evaluation issues if the result of this is used in many recursions.
 ;; possibly related: http://www.ksmpartners.com/2014/01/clojure-lazy-seq-and-the-stackoverflowexception/
@@ -45,35 +16,75 @@
   [p unmarked]
     (into [] (filter (fn [x] (not= (mod x p) 0)) unmarked))
     )
-
 (unmarkedFactors 3 [3 4 5 6 7 8 9])
 
 (defn divBy 
   [x y]
   (= (mod x y) 0))
 
+;;inclusive of n
 (defn sieve
   [n]
   (if (< n 2) 
     []
-    (loop [ [p & unmarked] (filter odd? (rest (rest (range n))))
-           final-unmarked () ]
+    (loop [ [p & unmarked] (filter odd? (range 2 (inc n)))
+           final-unmarked [] ]
       (do
-        (if (or (nil? p))
+        (if (nil? p)
           final-unmarked
           (recur (unmarkedFactors p unmarked)  (conj final-unmarked p)))))))
 
-(sieve 13059)
-(sieve 13999)
-(take 12 (sieve 139999))
-(take 12 (sieve 999999))
+;;segment includes the segmentSize - segmentStart element.  eg if the
+;; values are 3 4 respectively, includes 3 4 5 6 
+(defn sieveSegment
+  [primesUpToSqrtMax segmentStart segmentSize]
+   (loop [[prime & remain] primesUpToSqrtMax 
+          final-segment (into [] (filter odd? (range segmentStart (+ segmentStart segmentSize))))]
+      (if (nil? prime)
+        final-segment
+        (recur remain (crossOffMultiples prime final-segment)))))
 
-(take 12 (sieve 9999999))
-(take 12 (sieve 600851475))
-(take 12 (sieve 600851475143))
+(sieveSegment [3,5,7] 8 7.0)
 
-(take 12 (largestPrimeFactor 600851475143))
-(take 12 (largestPrimeFactor 60085147514))
+(defn segmentedSieve
+  [primesThrough]
+  (do
+   (if (< primesThrough 50)
+    (sieve primesThrough)
+    (let [segSize (int (math/floor (math/expt primesThrough 0.5)))
+          baseprimes (segmentedSieve segSize) ]
+      (do
+       (loop [segStart (inc segSize)  
+             result baseprimes ]  
+        (do
+          (if (> segStart primesThrough)
+           result
+           (if (< (- primesThrough segStart) segSize)
+             (recur (+ segStart (inc segSize)) (into result (sieveSegment baseprimes segStart (- primesThrough segStart))))
+             (do
+              (recur (+ segStart (inc segSize)) (into result (sieveSegment baseprimes segStart segSize)))))))))))))
+(sieve 70)
+(segmentedSieve 70)
+(sieve 444)
+(segmentedSieve 444)
+(sieve 99999)
+(segmentedSieve 30000)
+(segmentedSieve 99999)
+
+;;cross off multiples of p starting from p^.  assuming that segment is odds only
+(defn crossOffMultiples
+  [p segment]
+  (loop [check (* p p)
+         crossed-segment segment]
+    (if (<= check (last segment))
+     (if (= (mod check p) 0)
+       (recur (+ check 2) (into [] (remove (fn [x] (= x check)) crossed-segment)))
+       (recur (+ check 2) crossed-segment))
+     crossed-segment)))
+(crossOffMultiples 5 (filter odd? (range 10 80)))
+
+;;GC Error
+(take 12 (range 99999999))
 
 
 (defn largestPrimeFactor
@@ -87,18 +98,6 @@
         (if (and (divBy n first) (> first currentMax))
           (recur restPrimesLessThanN first)
           (recur restPrimesLessThanN currentMax))))))
-
 (largestPrimeFactor 600851475143)
 (largestPrimeFactor 19873)
-(largestPrimeFactor 29843)
-
-(unmarkedFactors 2 [3])
-
-
-( (fn 
-    [[p & others]]
-    (do
-      (println (str "p " p " and others " others )))) [] )
-
-(unmarkedFactors 4 [3 4 5 6 7 8 9 10 11 12])
 
